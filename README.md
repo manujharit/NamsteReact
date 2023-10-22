95811fe49dfb30e9907fff9139fb425f965ad2bc
manujharit <manuj.haritwal01@gmail.com>
2023-10-22 15:39:51 +0530

    ep-02

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# NamsteReact
+# Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot module replacement
+- File Watching Algorithm - written by c++
+- Faster Build bcoz of Caching
+- Image Optimization
+- Minification of file (bundling)
+- Compressing
+- Consistent Hashing 
+- Code splitting
+- Differential bundling - to support older browsers
+- Diagnostics
+- Error handling
+- https
+- Tree Shaking - remove unused code
+- Differect dev and prod bundles
--- miscellaneous/app.js
+++ miscellaneous/app.js
@@ -1,7 +1,10 @@
-const heading1 = React.createElement("h1", { id: "heading"}, "Hello World from React!")
-const heading2 = React.createElement("h2", { id: "heading"}, "Hello World from React!")
-const child = React.createElement("div",{id:"child"},[heading1,heading2])
-const parent = React.createElement("div",{id:"parent"},child)
-console.log(parent) // return object
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const heading1 = React.createElement("h1", { id: "heading" }, "Hello World from React!")
+const heading2 = React.createElement("h2", { id: "heading" }, "This is namaste react!")
+const child = React.createElement("div", { id: "child" }, [heading1, heading2])
+const parent = React.createElement("div", { id: "parent" }, child)
+// console.log(parent) // return object
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(parent)
